db73790cf1244e2a36e4ad1ee29b598cb129e513
EnTi <41371505+giaiaothoisu@users.noreply.github.com>
2024-08-11 07:33:42 +0700

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,7 +1,10 @@
+const express = require("express");
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const scrapeLogic = async (res) => {
+const app = express();
+
+const scrapeLogic = async (url) => {
   const browser = await puppeteer.launch({
     args: [
       "--disable-setuid-sandbox",
@@ -23,26 +26,24 @@ const scrapeLogic = async (res) => {
     await page.setViewport({ width: 1080, height: 1024 });
 
     // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+    // await page.type(".search-box__input", "automate beyond recorder");
 
     // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    // const searchResultSelector = ".search-box__link";
+    // await page.waitForSelector(searchResultSelector);
+    // await page.click(searchResultSelector);
 
     // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
+    const textSelector = await page.waitForSelector("title");
     const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
     // Print the full title
     const logStatement = `The title of this blog post is ${fullTitle}`;
     console.log(logStatement);
-    res.send(logStatement);
+    return logStatement;
   } catch (e) {
     console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
+    return `Something went wrong while running Puppeteer: ${e}`;
   } finally {
     await browser.close();
   }
