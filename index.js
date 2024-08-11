d3081c6441f50b3cb3ef8aacf98650e71f4bf8ca
EnTi <41371505+giaiaothoisu@users.noreply.github.com>
2024-08-11 07:18:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
+// app.get("/scrape", (req, res) => {
+//   scrapeLogic(res);
+// });
+
+app.get("/scrape", async (req, res) => {
+  const url = req.query.link;
+  if (!url) {
+    res.status(400).send("Missing URL parameter");
+    return;
+  }
+  const result = await scrapeLogic(url);
+  res.send(result);
 });
 
 app.get("/", (req, res) => {
